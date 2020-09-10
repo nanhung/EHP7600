f20c7ebfed41a4c7c2eef5b6c9890bd6028c77b1
Nan-Hung Hsieh <25087463+nanhung@users.noreply.github.com>
2020-09-10 10:23:15 -0700

    Update OneClickRun.R

--- OneClickRun.R
+++ OneClickRun.R
@@ -12,21 +12,34 @@ cores <- 1
 source("codes/0_mcmc_ind_chems.R")
 Sys.time()
 Sys.time() - Str.time 
+
 source("codes/1_mcmc_mixtures.R")
 Sys.time()
 Sys.time() - Str.time 
+
 source("codes/2_ec10_pred.R")
 Sys.time()
 Sys.time() - Str.time 
+
 source("codes/3_conc_resp_pred.R")
 Sys.time()
 Sys.time() - Str.time 
+
 source("codes/4_plot.R")
 Sys.time()
 Sys.time() - Str.time 
+
 source("codes/5_plot_suppl.R")
 Sys.time()
+Sys.time() - Str.time 
+
+#render('manuscript.Rmd')
+#Sys.time()
+#Sys.time() - Str.time 
+
+render('supplementary.Rmd')
+Sys.time()
+Sys.time() - Str.time
 
 cat(paste0("\nStarting time: ", Str.time), "\n\n")
 cat(paste0("\nEnding time: ", Sys.time(), "\n\n"))
-Sys.time() - Str.time
